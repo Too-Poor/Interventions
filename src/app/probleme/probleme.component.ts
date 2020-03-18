2d344a0dff6021a07f689dcf83c23f522a079ab6
Salif Diarra <salif@live.ca>
2020-03-18 19:23:05 -0400

    Créer le service typeprobleme

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ZonesValidator } from '../shared/longueur-minimum/longueur-minimum.component';
+import { ITypeProbleme } from './typeprobleme';
+import { TypeproblemeService } from './typeprobleme.service';
 
 @Component({
   selector: 'Inter-probleme',
@@ -10,13 +12,20 @@ import { ZonesValidator } from '../shared/longueur-minimum/longueur-minimum.comp
 export class ProblemeComponent implements OnInit {
   
   problemeForm: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  typesProbleme: ITypeProbleme[];
+  errorMessage: string;
+  constructor(private fb: FormBuilder, private types: TypeproblemeService) { }
 
   ngOnInit(): void {
     this.problemeForm = this.fb.group({
       prenom: ['', [ZonesValidator.longueurMinimum(3), Validators.required]],
-      nom: ['', [ZonesValidator.longueurMinimum(1), ZonesValidator.longueurMaximum(50), Validators.required]]
+      nom: ['', [ZonesValidator.longueurMinimum(1), ZonesValidator.longueurMaximum(50), Validators.required]],
+      typeProbleme: ['']
     });
+
+    this.types.obtenirTypeProbleme()
+    .subscribe(type => this.typesProbleme = type,
+               error => this.errorMessage = <any>error); 
   }
 
 }
